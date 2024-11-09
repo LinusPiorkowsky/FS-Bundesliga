1ddb8ed5d3f6dc824090fe928a1c1b4722476ae7
HenryLuekewille <s_luekewille23@stud.hwr-berlin.de>
2024-11-09 15:05:04 +0100

    move sql folder to instance folder

--- Sql/create_tables.sql
+++
@@ -1,9 +0,0 @@
--- Drop the users table if it exists
-DROP TABLE IF EXISTS users;
-
--- Create the users table
-CREATE TABLE users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT UNIQUE NOT NULL,
-    password TEXT NOT NULL
-);
