62c0e34ba0dd43e27ad2e007da1f90e99e48ffb4
LinusPiorkowsky <s_piorkowsky23@stud.hwr-berlin.de>
2025-01-15 17:50:34 +0100

    Performace logic adjustement

--- app.py
+++ app.py
@@ -635,7 +635,7 @@ def get_team_insights(team_name):
     highest_win_detail = f"{highest_win_score} vs {highest_win_opponent}"
     
     # Overall rating
-    rating = round((avg_goals_scored * 10 - avg_goals_conceded * 5 + clean_sheets * 10), 2)
+    rating = round((avg_goals_scored * 15 + total_shots_on_target * 10 - avg_goals_conceded * 20 + clean_sheets * 7), 2)
     rating = min(max(int(rating), 1), 100)
     
     return {
